d9a4df4ee4c3adfc9bdcb56cb313bb2af7af7752
lizzybera <elizabethokorie407@gmail.com>
2025-06-17 22:04:49 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,71 @@
+import fs from 'fs'
+import path from 'path'
+import os from "os"
+import http from "http"
+
+console.log("");
+
+
+// // read a file
+// fs.readFile('./random.txt', "utf8", (err:any, data : string) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+
+//     console.log("data: ", data);
+// })
+
+// // write in a file
+// try {
+//     fs.writeFileSync('./write', "Hello Backend engr, This is node")
+// } catch (error) {
+//     console.log(error);
+// }
+
+
+// const pathoffile = path.join(__dirname, 'public', "./write.txt")
+// console.log("pathoffile: ", pathoffile);
+
+
+// // console.log(os.cpus());
+
+
+
+// creating a server
+const port : number = 5690
+const port2 : number = 5000
+
+const server = http.createServer((req: http.IncomingMessage, res:http.ServerResponse<http.IncomingMessage>) => {
+    if (req.url === "/hello") {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end("welcome to Node.js")
+    } else {
+        res.writeHead(404)
+        res.end("Data Not found")
+    }
+})
+
+interface user {
+    id : number;
+    name : string
+}
+
+let users :user[] = [{id: 1, name: "liz"}, {id: 2, name : "Daniel"}]
+
+const server2 = http.createServer((req: http.IncomingMessage, res: http.ServerResponse<http.IncomingMessage>) => {
+    if(req.url === "/users"){
+        res.end(JSON.stringify(users))
+        return
+    }
+})
+
+// listen to the server on port
+server.listen(port, () =>{
+    console.log(`Server is listening ${port}`);
+} )
+
+server2.listen(port2, () =>{
+    console.log(`Server is listening on ${port2}`);
+})
+
