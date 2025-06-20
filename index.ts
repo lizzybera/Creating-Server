d4a2af39e519085b56cdbd724b850dace560532d
lizzybera <elizabethokorie407@gmail.com>
2025-06-20 21:48:41 +0100

    Methods

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 import os from "os"
-import http from "http"
+import http, { METHODS } from "http"
+import { json } from 'stream/consumers';
 
 console.log("");
 
@@ -33,10 +34,10 @@ console.log("");
 
 
 // creating a server
-const port : number = 5690
-const port2 : number = 5000
+const port: number = 5690
+const port2: number = 5000
 
-const server = http.createServer((req: http.IncomingMessage, res:http.ServerResponse<http.IncomingMessage>) => {
+const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse<http.IncomingMessage>) => {
     if (req.url === "/hello") {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end("welcome to Node.js")
@@ -47,25 +48,87 @@ const server = http.createServer((req: http.IncomingMessage, res:http.ServerResp
 })
 
 interface user {
-    id : number;
-    name : string
+    id: number;
+    name: string
 }
 
-let users :user[] = [{id: 1, name: "liz"}, {id: 2, name : "Daniel"}]
+let users: user[] = [{ id: 1, name: "liz" }, { id: 2, name: "Daniel" }]
 
 const server2 = http.createServer((req: http.IncomingMessage, res: http.ServerResponse<http.IncomingMessage>) => {
-    if(req.url === "/users"){
+    if (req.url === "/users") {
         res.end(JSON.stringify(users))
         return
+    }
+
+    // create a user
+    if (req.method === 'POST' && req.url === '/create') {
+        let body = ''
+
+        req.on('data', chunk => {
+            body += chunk
+        })
+
+        req.on('end', () => {
+            try {
+                const data = JSON.parse(body)
+                const newUser: user = {
+                    id: users.length + 1,
+                    name: data.name
+                }
+
+                users.push(newUser)
+
+                res.writeHead(201, { 'Content-Type': 'application/json' })
+
+                res.end(JSON.stringify(users))
+            } catch (err) {
+                res.writeHead(404, { 'Content-Type': 'application/json' })
+
+                res.end(JSON.stringify({ error: "Not Found" }))
+            }
+        })
+    }
+
+    // login
+    if (req.method === "POST", req.url === "/login") {
+        let body = ''
+        try {
+            req.on('data', chunk => {
+                body += chunk
+            })
+
+            req.on("end", () => {
+                const data = JSON.parse(body)
+
+                let check = users.map((el) => {
+                    return el
+                }).filter((el) => {
+                    return el.name === data.name
+                })
+                if (check.length === 0) {
+                    res.writeHead(404, { 'Content-Type': 'application/json' })
+
+                    res.end(JSON.stringify({ error: "Not Found" }))
+                }else{
+                    res.writeHead(201, { 'Content-Type': 'application/json' })
+
+                    res.end(`Welcome ${data.name}`)
+                }
+            })
+        } catch (error) {
+            res.writeHead(404, { 'Content-Type': 'application/json' })
+
+            res.end(JSON.stringify({ error: "Not Found" }))
+        }
     }
 })
 
 // listen to the server on port
-server.listen(port, () =>{
+server.listen(port, () => {
     console.log(`Server is listening ${port}`);
-} )
+})
 
-server2.listen(port2, () =>{
+server2.listen(port2, () => {
     console.log(`Server is listening on ${port2}`);
 })
 
